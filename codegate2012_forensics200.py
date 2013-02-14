18b46fb010430804ac6648ff12ebbd23804b50a5
Kenta Komai <kaede06152000@gmail.com>
2013-02-14 23:27:05 +0900

    add forensics200

---
+++ codegate2012_forensics200.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+import pytz
+from datetime import datetime
+import re
+
+now = int(str(1329009797205)[0:10])
+dt = datetime.fromtimestamp(now, pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%dT%H:%M:%S%z')
+index = re.search('\+([0-9]{1}){4}', dt)
+st = index.start()
+ed = index.end()
+dt = dt[0:st+3] + ':' + dt[st+3:ed]
+print '1_UNI/**/ON_SELECT|'+str(dt)
+
