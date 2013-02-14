b5ba0a423e54c80b49ff4c98c44cebdfa90faded
Kenta Komai <kaede06152000@gmail.com>
2013-02-15 01:07:10 +0900

    change timezone Tokyo->Seoul

--- codegate2012_forensics200.py
+++ codegate2012_forensics200.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import re
 
 now = int(str(1329009797205)[0:10])
-dt = datetime.fromtimestamp(now, pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%dT%H:%M:%S%z')
+dt = datetime.fromtimestamp(now, pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%dT%H:%M:%S%z')
 index = re.search('\+([0-9]{1}){4}', dt)
 st = index.start()
 ed = index.end()
