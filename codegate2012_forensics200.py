d2f982f21cb416cfdf6b0db1d918f7aa7220bfe0
Kenta Komai <kaede06152000@gmail.com>
2013-02-15 01:18:48 +0900

    change re pattern

--- codegate2012_forensics200.py
+++ codegate2012_forensics200.py
@@ -6,7 +6,7 @@ import re
 
 unixTime = int(str(1329009797205)[0:10])
 dt = datetime.fromtimestamp(unixTime, pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%dT%H:%M:%S%z')
-index = re.search('\+([0-9]{1}){4}', dt)
+index = re.search('\+[0-9]{4}', dt)
 st = index.start()
 ed = index.end()
 dt = dt[0:st+3] + ':' + dt[st+3:ed]
